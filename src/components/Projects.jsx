e1910cf6b51f4cd2cec58162fb53e7fed71b4799
Yusra Aman <Yusra.aman00@gmail.com>
2025-08-11 18:37:34 +0200

    Icons added for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,40 @@ import quizImg from '../assets/images/quiz.png';
 import weatherImg from '../assets/images/väder.png';
 import recipeImg from '../assets/images/recept.png';
 import animalImg from '../assets/images/djur.png';
+import individualIcon from '../assets/images/user.png';
+import groupIcon from '../assets/images/group.png';
+import teamImg from '../assets/images/teamwork.png';
 
 const Projects = () => {
   const projectArr = [
+    {
+      title: 'Lilla Djurvärlden',
+      description: 'En app för barn mellan 3-5 år där de får matcha rätt djurnamn med rätt djur',
+      tech: [
+          {name: 'HTML', icon: htmlIcon},
+          {name: 'CSS', icon: cssIcon },
+          {name: 'React', icon: reactIcon }
+        ],
+      link: 'https://lilla-djurvarlden.netlify.app',
+      image: animalImg,
+      teamIcon: groupIcon,
+      teamTitle: 'Teamwork'
+    },
+
+    {
+      title: 'Matrecept app',
+      description: 'App för matrecept som hämtar data genom Rest-API från mealDB',
+      tech: [
+          {name: 'HTML', icon: htmlIcon},
+          {name: 'CSS', icon: cssIcon },
+          {name: 'React', icon: reactIcon }
+        ],
+      link: 'https://food-recipes-live.netlify.app/',
+      image: recipeImg,
+      teamIcon: individualIcon,
+      teamTitle: 'Individual task'
+    },
+
     {
       title: 'Quiz app',
       description: 'Quiz app med några frågor',
@@ -19,37 +50,22 @@ const Projects = () => {
         ],
       link: 'https://quiz-game-live.netlify.app',
       image: quizImg,
-      team: 'individuellt'
+      teamIcon: individualIcon,
+      teamTitle: 'Individual task'
     }, 
 
     {
       title: 'Väder app',
       description: 'Hämtar väder med API från openMeteo',
       tech: [
-        { name: 'React', icon: reactIcon }, 
-        { name: 'CSS', icon: cssIcon }
+        { name: 'HTML', icon: htmlIcon },
+        { name: 'CSS', icon: cssIcon },
+        { name: 'React', icon: reactIcon }
       ],
       link: 'https://weather-broadcasting.netlify.app',
       image: weatherImg,
-      team: 'individuellt'
-    },
-
-    {
-      title: 'Matrecept app',
-      description: 'App för matrecept som hämtar data genom Rest-API från mealDB',
-      tech: ['React', 'CSS'],
-      link: 'https://food-recipes-live.netlify.app/',
-      image: recipeImg,
-      team: 'individuellt'
-    },
-
-    {
-      title: 'Lilla Djurvärlden',
-      description: 'En app för barn mellan 3-5 år där de får matcha rätt djurnamn med rätt djur',
-      tech: ['React', 'CSS'],
-      link: 'https://lilla-djurvarlden.netlify.app',
-      image: animalImg,
-      team: 'grupp'
+      teamIcon: individualIcon,
+      teamTitle: 'Individual task'
     }
   ]
 
@@ -63,18 +79,18 @@ const Projects = () => {
       <section id="project-cards">
         {projectArr.map((projects, index) =>(
           <div key={index} className="project-box"  onClick={() => window.open(projects.link, '_blank')}>
-            <h3>{projects.title}</h3><br></br>
+            <h3>{projects.title}</h3>
             <img
               src={projects.image}
               alt={projects.title}
               className='project-img'
             />
             <p>{projects.description}</p>
-            <p className='team'>{projects.team}</p>
-
-          <div className='tech-icons'>
+        
+        <div className='project-icons-container'>
+            <div className='tech-icons'>
             {projects.tech.map((t, i) =>(
-              <img
+            <img
               key={i}
               src={t.icon}
               alt={t.name}
@@ -82,8 +98,16 @@ const Projects = () => {
               className='tech-icon'
               />
             ))}
-          </div>
         </div>
+
+            <img
+              src={projects.teamIcon}
+              alt={projects.teamTitle}
+              title={projects.teamTitle}
+              className='team-icon'
+              />
+            </div>
+          </div>
       ))}
       </section>
     </div>
